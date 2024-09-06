ce8edc6728d7b0d7543d9edf923421514c3f0864
burakcagriaktas <burakcagriaktas@hotmail.com>
2024-09-06 20:25:53 +0300

    initial commit for broker firm app with in memory db h2

---
+++ src/main/java/com/ing/broker/BrokerAppApplication.java
@@ -0,0 +1,13 @@
+package com.ing.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrokerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ing/broker/BrokerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
